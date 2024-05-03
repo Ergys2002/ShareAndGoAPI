35198f6f893db436b20b4e55ba6e525411711382
Ergys <ergysxhaollari02@gmail.com>
2024-05-03 16:10:48 +0200

    initial commit

---
+++ src/main/java/com/app/ShareAndGo/ShareAndGoApplication.java
@@ -0,0 +1,13 @@
+package com.app.ShareAndGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareAndGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareAndGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/ShareAndGo/ShareAndGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.ShareAndGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareAndGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
